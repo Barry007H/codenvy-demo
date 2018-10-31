100baedf35e66dcbd43ebf5320950efdee9c14df
Kryptyk Physh <kryptykphysh@kryptykphysh.xyz>
2018-10-31 23:19:17 +0000

    Config complete.

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,13 @@ module Webapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    # Use UUIDs as primary keys
+    # config.generators do |g|
+    #   g.orm :active_record, primary_key_type: :uuid
+    # end
+
+    # Use SQL in schema
+    config.active_record.schema_format = :sql
   end
 end
